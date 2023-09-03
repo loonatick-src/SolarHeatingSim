8467a1e062561d70dc96fdfae9641dc0de6dcd27
Chaitanya Kumar <chaitanyak28771@gmail.com>
2023-09-03 13:45:12 +0200

    Compiling and running now, but getting NaNs

--- src/Simulation.jl
+++ src/Simulation.jl
@@ -21,21 +21,52 @@ struct SWHSModel{T}
     Vt::T   # volume of each stratified tank layer
     Ac::T   # transverse cross section area of risers
     ρAv::T  # mass flow rate of water
-    Ut::T   # loss coefficient (storage tank)
+    hta::T   # loss coefficient (storage tank)
     hpf::T  # plate-fluid heat transfer coefficient
     hpa::T  # plate-atmosphere transfer coefficient
     Ns::Int # stratifications count
 
     # ctor with default arguments
-    function SWHSModel(;S = 800.0, Ta = 300, T∞ = 295, α = 5.5e-8,
-                       δ = 0.1, W = 1.0, L = 2.0, Cpl = 4.2e3,
-                       Cpp = 450.0, Cpt = 5000.0, ρl = 1.0e3, ρp = 8.0e3,
-                       ρt = 1.5e3, Vt = 10.0, Ac = 0.6, ρAv = 800.0, Ut = 500.0,
-                       hpf = 1000.0, hpa = 100.0, Ns = 10)
-        new{Float64}(S, Ta, T∞, α, δ, W,
-                     L, Cpl, Cpp, Cpt,
-                     ρl, ρp, ρt, Vt, ρAv,
-                     Ut, hpf, hpa, Ns)
+    function SWHSModel(;S      = 800.0,
+                       Ta      = 300,
+                       T∞      = 295,
+                       α       = 5.5e-8,
+                       δ       = 0.1,
+                       W       = 1.0,
+                       L       = 2.0,
+                       Cpl     = 4.2e3,
+                       Cpp     = 450.0,
+                       Cpt     = 5000.0,
+                       ρl      = 1.0e3,
+                       ρp      = 8.0e3,
+                       ρt      = 1.5e3,
+                       Vt      = 10.0,
+                       Ac      = 0.6,
+                       ρAv     = 800.0,
+                       hta     = 500.0,
+                       hpf     = 1000.0,
+                       hpa     = 100.0,
+                       Ns::Int = 10)
+        new{Float64}(S,
+                     Ta,
+                     T∞,
+                     α,
+                     δ,
+                     W,
+                     L,
+                     Cpl,
+                     Cpp,
+                     Cpt,
+                     ρl,
+                     ρp,
+                     ρt,
+                     Vt,
+                     Ac,
+                     ρAv,
+                     hta,
+                     hpf,
+                     hpa,
+                     Ns)
     end
 end
 
@@ -54,19 +85,23 @@ struct SWHSProblem{T}
     Tp0::T
     function SWHSProblem(model::SWHSModel; Nc = 100, Δt = 0.1, Tf0 = 290.0, Tp0 = 300.0)
         T = value_type(model)
-        @unpack Cpp, ρp, δ, L, Nc, Ns = model
-        @unpack W, hpf, ρf, Ac, Cpl = model
+        @unpack Cpp, ρp, δ, L, Ns, ρAv, Vt, Cpt = model
+        @unpack W, hpf, ρl, Ac, Cpl, ρt, hta = model
+        A = 4.0  # TODO: put this in the model
         Δy = L / Nc
         c1 = Cpp * ρp * δ
-        c2 = W*hpf/(ρf * Ac * Cpl)
+        c2 = W*hpf/(ρl * Ac * Cpl)
         c3 = ρAv / (ρt*Vt)
         c4 = hta * A/(ρt * Vt * Cpt)
         N = 2Nc + model.Ns
-        new{Float64}(Δy, Δt, Nc, c1, c2, c3, c4, Tf0, Tp0)
+        new{Float64}(model, Δy, Δt, Nc, c1, c2, c3, c4, Tf0, Tp0)
     end
 end
 
 function step!(timeseries_data, prob::SWHSProblem, i)
+    @unpack Δt, Δy, Nc, c1, c2, c3, c4 = prob
+    m = prob.model
+    @unpack α = m
     Tprev = @view timeseries_data[:,i-1]
     Tcurr = @view timeseries_data[:,i]
     Tp_prev = @view Tprev[begin:begin+Nc-1]
@@ -82,13 +117,10 @@ function step!(timeseries_data, prob::SWHSProblem, i)
     @assert length(Tl_prev_sl) == length(Tl_prev)
     Tf_prev_sl = @view Tprev[Nc:2Nc-1]
     Tf_prev_sr = @view Tprev[Nc+2:2Nc+1]
-    @assert length(Tf_prev_sl) == length(Tf_prev_sr) == lenght(Tf_prev)
+    @assert length(Tf_prev_sl) == length(Tf_prev_sr) == length(Tf_prev)
     # you should see a one-to-one correspondence with the fully discretized equations in the README.md
-
-    @unpack Δt, Δy, Nc, c1, c2 = prob
-    m = prob.model
     @unpack S, hpf, hpa, Ta, T∞ = m
-    Tp_curr .= Tp_prev .+ (Δt / c1) .* (S .- hpf .* (Tp_prev .- Tf_prev) - hpa .* (Tp_prev .- Ta) - α .* (Tp_prev.^4 - T∞.^4))
+    Tp_curr .= Tp_prev .+ (Δt / c1) .* (S .- hpf .* (Tp_prev .- Tf_prev) .- hpa .* (Tp_prev .- Ta) .- α .* (Tp_prev.^4 .- T∞.^4))
     Tf_curr .= Tf_prev .+ Δt .* (c2 .* (Tp_prev .- Tf_prev) - (Tf_prev_sr .- Tf_prev_sl) ./ 2Δy)
     Tl_curr .= Tl_prev .+ Δt .* (c3 .* (Tl_prev_sl .- Tl_prev) .- c4 .* (Tl_prev .- Ta))
     timeseries_data
@@ -97,6 +129,7 @@ end
 function solve(prob::SWHSProblem; maxiter = prob.Nc^2)
     @unpack model, Nc, Tp0, Tf0 = prob
     @unpack Ns = model
+    A = 4.0  # TODO: put this in the model
     N = 2Nc + Ns
     timeseries_data = zeros(N,maxiter)
     timeseries_data[begin:begin+Nc-1,:] .= Tp0
